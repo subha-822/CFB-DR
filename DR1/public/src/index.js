ce52c201afb2d93aa55a25dc6292928980bb755e
SUBHA RAJAGOPAL <78970529+subha-822@users.noreply.github.com>
2022-03-25 19:52:32 +0530

    some changes made

---
+++ DR1/public/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Happy Journey</h1>
+    <p>Happy Life</p>
+  </div>,
+  document.getElementById("root")
+);
