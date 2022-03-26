385e56a4ce5e1c308bde16ee95fb998ad6370d63
SUBHA RAJAGOPAL <78970529+subha-822@users.noreply.github.com>
2022-03-26 17:45:47 +0530

    changes

--- DR1/public/src/index.js
+++ DR1/public/src/index.js
@@ -1,37 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-/* 1st Part */
-// ReactDOM.render(
-//   <div>
-//     <h1>Happy Journey</h1>
-//     <p>Happy Life</p>
-//   </div>,
-//   document.getElementById("root")
-// );
-
-/* 2nd Part */
-// ReactDOM.render(
-//   <div>
-//     <h1>My Favourite Foods</h1>
-//     <ul>
-//       <li>Bacon</li>
-//       <li>Jamon</li>
-//       <li>Noodles</li>
-//     </ul>
-//   </div>,
-//   document.getElementById("root")
-// );
-
-/* 3rd Part */
-const name = "Angela";
-const number = 2;
 ReactDOM.render(
   <div>
-    <h1>Hello {name}!</h1>
-    <p>Your lucky number is {number}.</p>
-    <p>Your lucky number is {Math.random()}.</p>
-    <p>Your lucky number is {Math.floor(Math.random() * 10)}.</p>
+    <h1>Happy Journey</h1>
+    <p>Happy Life</p>
   </div>,
   document.getElementById("root")
 );
