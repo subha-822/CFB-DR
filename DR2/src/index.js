917b41cc8b4c7c3469bef236275116344db075a8
SUBHA RAJAGOPAL <78970529+subha-822@users.noreply.github.com>
2022-03-26 17:44:38 +0530

    some codes added

---
+++ DR2/src/index.js
@@ -0,0 +1,39 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+/* 1st Part */
+
+// const name = "subha";
+// const year = 2022;
+// ReactDOM.render(
+//   <div>
+//     <p>Created by {name}</p>
+//     <p>Copyright {year}</p>
+//   </div>,
+//   document.getElementById("root")
+// );
+
+/* 2nd Part */
+
+// const name = "subha";
+// const currentDate = new Date();
+// const year = currentDate.getFullYear();
+
+// ReactDOM.render(
+//   <div>
+//     <p>Created by {name}</p>
+//     <p>Copyright {year}</p>
+//   </div>,
+//   document.getElementById("root")
+// );
+
+/* 3rd Part */
+
+// const img = "https://picsum.photos/200";
+// ReactDOM.render(
+//   <div>
+//     <h1>My Favourite Foods</h1>
+//     <img src={img} />
+//   </div>,
+//   document.getElementById("root")
+// );
