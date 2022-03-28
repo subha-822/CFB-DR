6325462bbb74150028796f8e887f9de3cdcf42ce
SUBHA RAJAGOPAL <78970529+subha-822@users.noreply.github.com>
2022-03-28 20:00:36 +0530

    components learn

---
+++ DR3/src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+ReactDOM.render(
+  /* <Heading></Heading>
+    <Heading />
+    <p>Happy Life</p>
+    <List /> */
+  <App />,
+  document.getElementById("root")
+);
+
+// function Heading() {
+//   return <h1>Happy Journey</h1>;
+// }
