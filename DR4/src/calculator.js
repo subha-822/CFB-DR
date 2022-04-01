761697a2e9a4df0eff4b38f5cebbd19f88e25420
SUBHA RAJAGOPAL <78970529+subha-822@users.noreply.github.com>
2022-04-01 17:20:31 +0530

    cal

---
+++ DR4/src/calculator.js
@@ -0,0 +1,14 @@
+function add(n1, n2) {
+  return n1 + n2;
+}
+function sub(n1, n2) {
+  return n1 - n2;
+}
+function multiply(n1, n2) {
+  return n1 * n2;
+}
+function divide(n1, n2) {
+  return n1 / n2;
+}
+
+export { add, sub, multiply, divide };
---
+++ DR4/src/index.js
@@ -0,0 +1,36 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+/* 1st Part */
+//import { add, sub, multiply, divide } from "../src/calculator";
+// ReactDOM.render(
+//   <div>
+//     <li>{add(1, 2)}</li>
+//     <li>{sub(7, 2)}</li>
+//     <li>{multiply(2, 3)}</li>
+//     <li>{divide(5, 2)}</li>
+//   </div>,
+//   document.getElementById("root")
+// );
+
+/* 2nd part */
+// import * as calculator from "../src/calculator";
+
+// ReactDOM.render(
+//   <div>
+//     <li>{calculator.add(1, 2)}</li>
+//     <li>{calculator.sub(7, 2)}</li>
+//     <li>{calculator.multiply(2, 3)}</li>
+//     <li>{calculator.divide(5, 2)}</li>
+//   </div>,
+//   document.getElementById("root")
+// );
+
+/* 3rd Part */
+
+ReactDOM.render(
+  <div>
+    <li>Hai</li>
+  </div>,
+  document.getElementById("root")
+);
