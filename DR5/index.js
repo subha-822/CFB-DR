13ea9510e253c132f646d9bfe0aebbd91927e85c
SUBHA RAJAGOPAL <78970529+subha-822@users.noreply.github.com>
2022-04-05 16:59:00 +0530

    jquery learned

---
+++ DR5/index.js
@@ -0,0 +1,31 @@
+// $("h1").text("Happy.");
+
+// $("h1").css("color","green");
+// $("h1").css("font-size","10rem");
+
+// $("h1").addClass("big-title margin");
+// $("h1").removeClass("big-title");
+
+// $("h1").click(function() {
+//     $("h1").css("color","purple");
+// })
+
+// $("h1").before("<button>");
+// $("h1").after("<button>");
+// $("h1").prepend("<button>");
+// $("h1").append("<button>");
+
+// $("button").on("click",function(){
+//     // $("h1").hide();
+//     // $("h1").show();
+//     // $("h1").fadeOut();
+//     // $("h1").fadeIn();
+//     // $("h1").toggle();
+//     // $("h1").fadeToggle();
+//     // $("h1").slideUp();
+//     // $("h1").slideDown();
+//     // $("h1").slideToggle();
+//     // $("h1").animate({opacity:0.5});
+//     // $("h1").animate({margin:"20%"});
+//     $("h1").slideUp().slideDown.animate({opacity:0.5});
+// });
